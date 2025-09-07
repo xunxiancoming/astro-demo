e1de81c3e2fb80b682b72fce674abb0ace56a451
jinlong <1475711781@qq.com>
2025-09-07 23:12:26 +0800

    自定义

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kblog2.netlify.app",
-  integrations: [preact()],
+  integrations: [
+    preact({ include: "**/preact/*" }),
+    react({ include: "**/react/*" }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
