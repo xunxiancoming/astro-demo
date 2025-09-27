3683eb0e025507cfffa834c24dc8586ae38d54c2
jinlong <1475711781@qq.com>
2025-09-27 13:15:05 +0800

    github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kblog2.netlify.app",
+  site: "https://xuanxiancoming.github.io",
   integrations: [
     preact({ include: "**/preact/*" }),
     react({ include: "**/react/*" }),
